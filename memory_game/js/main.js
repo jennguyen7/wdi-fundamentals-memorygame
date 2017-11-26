45c7e03d805232013d1e9e1e1c0ee7e2f6d64e85
Jen <jen@gmail.com>
2017-11-26 14:56:12 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,34 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cards.push(cardOne);
+cards.push(cardTwo);
+cards.pop();
+cards.pop();
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped queen");
+console.log("User flipped king");
+
+
+if (cardsInPlay.length === 2) {
+	console.log("There are 2 cards");
+}
+else {
+	console.log("There aren't 2 cards");
+}
+
+if (cardsInPlay[0] === cardsInPlay[3]) {
+	alert("You found a match!");
+}
+else {
+	alert("Try again");
+}
